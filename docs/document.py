66691b464cc4a0fe8e52b3988f11ebccfe4932e1
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-17 19:54:56 -0500

    refactor: document script

--- docs/document.py
+++ docs/document.py
@@ -2,11 +2,12 @@ with open('README-TEMPLATE.md') as f:
     lines = f.readlines()
 
 variable_name = 'repo'
-variable_value = None
-
-for line in lines:
-    if line.startswith(f'[{variable_name}]'):
-        variable_value = line.split(':')[-1].strip()
-        break
-
+variable_value = next(
+    (
+        line.split(':')[-1].strip()
+        for line in lines
+        if line.startswith(f'[{variable_name}]')
+    ),
+    None,
+)
 print(variable_value)
